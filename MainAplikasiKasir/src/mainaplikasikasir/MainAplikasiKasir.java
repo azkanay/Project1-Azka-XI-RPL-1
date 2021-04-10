66f917ce6cd804a6d0fcc13755f74f3f7e524ca9
azkanay <atta.nayla@gmail.com>
2021-04-10 22:30:25 +0700

    Add Fitur Menambah dan Menampilkan Daftar Menu

--- MainAplikasiKasir/src/mainaplikasikasir/MainAplikasiKasir.java
+++ MainAplikasiKasir/src/mainaplikasikasir/MainAplikasiKasir.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 package mainaplikasikasir;
+import classes.DaftarMenu;
+import classes.Kuah;
+import classes.Minuman;
+import classes.Ramen;
+import classes.Topping;
 import java.util.Scanner;
 
 /**
@@ -16,13 +21,38 @@ public class MainAplikasiKasir {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Masukkan nama anda:");
-        String namaUser = scan.nextLine();
+        //inisialisasi kelas Scanner untuk mengambil
+        //input dari keyboard
+        Scanner input = new Scanner(System.in);
         
-        System.out.println("Nama anda adalah: " + namaUser);
+        MainAplikasiKasir app = new MainAplikasiKasir();
+        //tampilkan daftar menu
+        app.generateDaftarMenu();
+
     }
     
-public void generateDaftarMenu() { }
+public DaftarMenu daftarMenu;
+    
+public void generateDaftarMenu() { 
+    daftarMenu = new DaftarMenu();
+    daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
+    daftarMenu.tambahMenu(new Ramen("Ramen Original", 18000));
+    daftarMenu.tambahMenu(new Ramen("Ramen Vegetarian", 22000));
+    daftarMenu.tambahMenu(new Ramen("Ramen Karnivor", 28000));
+    daftarMenu.tambahMenu(new Kuah("Kuah Orisinil"));
+    daftarMenu.tambahMenu(new Kuah("Kuah Internasional"));
+    daftarMenu.tambahMenu(new Kuah("Kuah Spicy Lada"));
+    daftarMenu.tambahMenu(new Kuah("Kuah Soto Padang"));
+    daftarMenu.tambahMenu(new Topping("Crab Stick Bakar", 6000));
+    daftarMenu.tambahMenu(new Topping("Chicken Katsu", 8000));
+    daftarMenu.tambahMenu(new Topping("Gyoza Goreng", 4000));
+    daftarMenu.tambahMenu(new Topping("Bakso Goreng", 7000));
+    daftarMenu.tambahMenu(new Topping("Enoki Goreng", 5000));
+    daftarMenu.tambahMenu(new Minuman("Jus Alpukat SPC", 10000));
+    daftarMenu.tambahMenu(new Minuman("Jus Stroberi", 11000));
+    daftarMenu.tambahMenu(new Minuman("Capucino Coffee", 15000));
+    daftarMenu.tambahMenu(new Minuman("Vietnam Drip", 14000));
+    
+    daftarMenu.tampilDaftarMenu();
+  }
 }
